33cae843a1cb60472d852c4a5bfd66d5bccf7fc0
Wevison Ramalho Silva <wevison.ramalho@afferolab.com.br>
2021-04-01 12:24:55 -0300

    [ FEATURE ] Adding elements background.

--- dist/base.98fd6c19.js
+++ dist/base.98fd6c19.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\image\\background-page.jpg":[["background-page.15d6a284.jpg","image/background-page.jpg"],"image/background-page.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\image\\background-page.png":[["background-page.2ea351af.png","image/background-page.png"],"image/background-page.png"],"./..\\image\\roupas.png":[["roupas.c7a8febf.png","image/roupas.png"],"image/roupas.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61224" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62518" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
