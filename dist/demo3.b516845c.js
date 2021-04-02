a4a6777e330c7c6da15804fa88350d14c9dcb1cb
Wevison Ramalho Silva <wevison.ramalho@afferolab.com.br>
2021-04-02 17:58:50 -0300

    [ FEATURE ] Adding elements in the layout.

--- dist/demo3.b516845c.js
+++ dist/demo3.b516845c.js
@@ -5889,7 +5889,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65018" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49470" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
