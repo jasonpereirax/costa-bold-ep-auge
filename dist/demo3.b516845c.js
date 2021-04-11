0ba632305c76db309767fc6df995fcf50c92af18
Wevison Ramalho Silva <wevison.ramalho@afferolab.com.br>
2021-04-11 20:16:03 -0300

    [ FEATURE ] Adding integration with formspree.

--- dist/demo3.b516845c.js
+++ dist/demo3.b516845c.js
@@ -5890,7 +5890,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52990" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54479" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
